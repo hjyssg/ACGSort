411d69763008f27ef80e8ab06a61eae3acc99393
junyang huang <>
2013-07-12 17:30:55 -0400

    added UI

---
+++ src/mangamanagetool/SortedFileTable.java
@@ -0,0 +1,78 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangamanagetool;
+
+/**
+ *
+ * @author junyang_huang
+ */
+import java.io.*;
+import java.util.*;
+
+import java.net.*;
+import javax.swing.tree.ExpandVetoException;
+
+public class SortedFileTable
+{
+
+    /**
+     * the key is the author name e.g 鳥居姫, 魚骨工造 (カポ), 怪奇日蝕(綾野なおと)
+     * 
+     */
+    public Hashtable<String, AuthorInfo> table;
+
+    public SortedFileTable(String sortedFolderPath) throws Exception
+    {
+        this.table = new Hashtable<String, AuthorInfo>();
+
+        this.iterateAllSubfolder(new File(sortedFolderPath));
+    }
+
+    private void iterateAllSubfolder(File rootFolder)
+    {
+        try
+        {
+            //iterate through each author's folder
+            for (File f : rootFolder.listFiles())
+            {
+                //...and find author name, save into  ealist
+                if (f.isDirectory() && (!f.isHidden()))
+                {
+                    String directoryName = f.getName();
+
+                    AuthorInfo entry = new AuthorInfo();
+                    entry.names = NameParser.getAuthorNameEntry(directoryName);
+                    entry.directory = f;
+
+                    this.table.put(directoryName, entry);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
+    }
+
+   
+
+    public void debugDisplay()
+    {
+        ArrayList<String> arr = new ArrayList();
+        for (String s : table.keySet())
+        {
+            arr.add(s);
+        }
+
+        System.out.println("number of keys " + arr.size());
+
+        Collections.sort(arr);
+        for (String s : arr)
+        {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/mangamanagetool/UnsortedFileTable.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangamanagetool;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author junyang_huang
+ */
+public class UnsortedFileTable
+{
+    //the key is the author name 
+    //the value is the array of file of his/her manga and doujinshi
+   public Hashtable<String, AuthorInfo> table;
+
+    //create the authotList based on the text file from Everthing
+    public UnsortedFileTable(String fileFolderPath) throws Exception
+    {
+        table = new Hashtable<String, AuthorInfo>();
+
+        this.iterateAllSubfolderAndFindCompressedFile(new File(fileFolderPath));
+    }
+
+    private void iterateAllSubfolderAndFindCompressedFile(File dir)
+    {
+        try
+        {
+            File[] files = dir.listFiles();
+
+            if (files != null)
+            {
+                for (File f : files)
+                {
+                    if (f.isFile())
+                    {
+                        String fileName = f.getName();
+
+                        //get file extension
+                        String extension = NameParser.getFileExtension(fileName);
+
+                        if (!f.isHidden() && NameParser.isCompressionFile(extension))
+                        {
+                            String authorName = NameParser.getAuthorName(fileName);
+
+                            if (authorName != null)
+                            {
+                                //System.out.println(authorName+"  " + fileName);
+
+                                if (table.containsKey(authorName))
+                                {
+                                    table.get(authorName).files.add(f);
+                                }
+                                else
+                                {
+                                 
+                                    AuthorInfo entry = new AuthorInfo();
+                                    ArrayList fileEntry = new ArrayList<File>();
+                                    fileEntry.add(f);
+                                    entry.files = fileEntry;
+                                    entry.names = NameParser.getAuthorNameEntry(authorName);
+                                    
+                                    table.put(fileName, entry);
+                                    
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        iterateAllSubfolderAndFindCompressedFile(f);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void debugDisplay()
+    {
+        ArrayList<String> arr = new ArrayList();
+        for (String s : table.keySet())
+        {
+            arr.add(s);
+        }
+
+        System.out.println("number of keys " + arr.size());
+
+        Collections.sort(arr);
+        for (String s : arr)
+        {
+            System.out.println(s);
+        }
+    }
+
+}
